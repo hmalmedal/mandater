0409c8b6bd40ab4fcd353c304abf5ece9dd213be
Håkon Malmedal <hmalmedal@gmail.com>
2014-07-09 17:03:45 +0200

    Endring

--- server.R
+++ server.R
@@ -36,6 +36,11 @@ mandatfordelingsdata <- mandatfordelingsdata %>%
     group_by(Tid) %>%
     mutate(Mandater = saintelague(169, Folketall + 1.8 * Areal))
 
+pre2004 <- c(8, 15, 16, 8, 7, 7, 8, 6, 4, 5, 11, 17, 5, 10, 10, 6, 12, 6, 4)
+
+mandatfordelingsdata <- ungroup(mandatfordelingsdata) %>%
+    mutate(Endring = diff(c(pre2004, Mandater), lag = 19))
+
 shinyServer(function(input, output) {
     tabell <- reactive({
         ungroup(mandatfordelingsdata) %>%
