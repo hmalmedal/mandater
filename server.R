4e03e7dc60ba0a6aa3cdea02ccb9d9aef05870ab
Håkon Malmedal <hmalmedal@gmail.com>
2014-07-09 13:53:22 +0200

    Data for alle år

--- server.R
+++ server.R
@@ -7,6 +7,29 @@ mandatfordelingsdata <- read.csv("mandatfordelingsdata.csv",
 source("areal.R")
 source("folkemengde.R")
 
+fylker <- unique(mandatfordelingsdata$Fylke)
+
+areal <- areal %>%
+    mutate(fylkesnr = factor(fylkesnr,
+                             levels = unique(fylkesnr),
+                             labels = fylker)) %>%
+    select(Fylke = fylkesnr, Areal = areal)
+
+folk <- ungroup(folk) %>%
+    mutate(region = factor(region,
+                           levels = unique(region),
+                           labels = fylker)) %>%
+    select(Fylke = region, Folketall = folkemengde, Tid = tid)
+
+arealfolk <- inner_join(folk, areal)
+
+mandatfordelingsdata  <- mandatfordelingsdata %>%
+    mutate(Fylke = factor(Fylke, levels = unique(Fylke), labels = fylker))
+mandatfordelingsdata <- rbind(mandatfordelingsdata, arealfolk)
+
+mandatfordelingsdata  <- mandatfordelingsdata %>%
+    arrange(Tid, Fylke)
+
 shinyServer(function(input, output) {
     tabell <- reactive({
         mandatfordelingsdata %>%
