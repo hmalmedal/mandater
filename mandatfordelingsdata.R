7983921be223c4ad7851295e7304f0793954c6ea
Håkon Malmedal <hmalmedal@gmail.com>
2016-08-06 15:28:58 +0200

    col_types

--- mandatfordelingsdata.R
+++ mandatfordelingsdata.R
@@ -12,7 +12,8 @@ arealfolk <- inner_join(folk, areal, by = "Fylke") %>%
 source("saintelague.R")
 pre2004 <- c(8, 15, 16, 8, 7, 7, 8, 6, 4, 5, 11, 17, 5, 10, 10, 6, 12, 6, 4)
 
-mandatfordelingsdata <- read_csv("mandatfordelingsdata.csv") %>%
+mandatfordelingsdata <- read_csv("mandatfordelingsdata.csv",
+                                 col_types = "ciii") %>%
   bind_rows(arealfolk) %>%
   group_by(Tid) %>%
   mutate(Mandater = saintelague(169, Folketall + 1.8 * Areal)) %>%
